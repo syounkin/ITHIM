074f710d61d15b7c61335d1083ffcec9e295f862
Samuel Younkin <samuel.younkin@gmail.com>
2016-05-17 13:23:52 -0500

    Created functions.R for the first function

---
+++ R/functions.R
@@ -0,0 +1,14 @@
+convertAge <- function(value, unit){
+
+    value <- ifelse(value == "999", NA, value)
+
+    convertedValue <- ifelse( unit == "1", value,
+    ifelse( unit == "2", value/12,
+    ifelse( unit == "4", value/365,
+    ifelse( unit == "5", value/365/24,
+    ifelse( unit == "6", value/365/24/60,
+    ifelse( unit == "9", NA, 999 ))))))
+
+    return(as.numeric(convertedValue))
+
+}
