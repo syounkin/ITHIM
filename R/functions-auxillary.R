dddd7598612238583876bc287b36ac75f2cf19e8
Samuel Younkin <samuel.younkin@gmail.com>
2017-06-20 11:55:12 -0500

    Added functions to retrieve underlying distributions
    Namely, getDistribution, getDistribution2 and compareDistributions.  compareDistributions returns a ggplot object.  I am not entirely sure how this will behave.

--- R/functions-auxillary.R
+++ R/functions-auxillary.R
@@ -171,6 +171,59 @@ getTractAgeSex <- function(state, county) {
 }
 
 
+getDistribution2 <- function(x){
+
+    n <- ncol(x)
+
+    foobar <- melt(x)
+    names(foobar) <- c("age","k","value")
+    D <- foobar  %>% arrange(age) %>% cbind(.,F = cumsum(rep(1,n)/(n+1)))
+
+    foo <- c("00-04","05-14","15-29","30-44","45-59","60-69","70-79","80+")
+    names(foo) <- paste0("ageClass",1:8)
+
+
+    D <- within(D, age <- as.factor(foo[age]))
+
+    return(D)
+}
+
+
+
+getDistribution <- function(ITHIM, type = "TotalMET"){
+
+    foo <- ITHIM@quintiles[[type]]$M
+    D <- data.frame(sex = "M", getDistribution2(foo))
+
+    foo <- ITHIM@quintiles[[type]]$F
+    D <- rbind(D, data.frame(sex = "F", getDistribution2(foo)))
+
+    D <- within(D, age <-  as.factor(age))
+
+    return(D)
+}
+
+
+
+compareDistributions <- function(ITHIM.baseline, ITHIM.scenario, type = "TotalMET"){
+
+    D.baseline <- getDistribution(ITHIM.baseline, type = type)
+
+    D.scenario <- getDistribution(ITHIM.scenario, type = type)
+
+    D <- rbind(cbind(D.baseline, vision = "baseline"),cbind(D.scenario, vision = "scenario"))
+
+    D <- within(D, {
+        vision <-  as.factor(vision)
+        age <-  factor(age, levels = c("00-04","05-14","15-29","30-44","45-59","60-69","70-79","80+"))
+        sex <-  as.factor(sex)
+    })
+
+    p <- ggplot(D, aes(x = value, y = F, colour = vision))
+    p <- p + geom_line(aes(colour = vision)) + facet_grid( sex ~ age) + xlab(type) + ylab("Cumulative Distribution")
+
+    return(p)
+}
 
 
 
