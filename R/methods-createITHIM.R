f07fc6d4411c085d012bf0009967554cb2df18ab
Samuel Younkin <samuel.younkin@gmail.com>
2017-11-21 11:34:13 -0600

    createITHIM now expects exactly three files activeTransport, GBD and F.

--- R/methods-createITHIM.R
+++ R/methods-createITHIM.R
@@ -1,47 +1,54 @@
 #' @rdname createITHIM-methods
 #' @aliases createITHIM
 #' @export
-setMethod("createITHIM", signature(activeTransportFile = "character", GBDFile = "character", roadInjuriesFile = "character", distRoadTypeFile = "character", safetyInNumbersFile = "character"), function(activeTransportFile, GBDFile, roadInjuriesFile, distRoadTypeFile, safetyInNumbersFile){
-    ITHIM <- createITHIMFunction(activeTransportTimeFile = activeTransportFile, roadInjuriesFile = roadInjuriesFile, GBDFile = GBDFile, distRoadTypeFile = distRoadTypeFile, safetyInNumbersFile = safetyInNumbersFile)
+setMethod("createITHIM", signature(activeTransportFile = "character", GBDFile = "character", FFile = "character"), function(activeTransportFile, GBDFile, FFile){
+    ITHIM <- createITHIMFunction(activeTransportTimeFile = activeTransportFile, GBDFile = GBDFile, FFile = FFile)
     return(ITHIM)
 })
+## #' @rdname createITHIM-methods
+## #' @aliases createITHIM
+## #' @export
+## setMethod("createITHIM", signature(activeTransportFile = "character", GBDFile = "character", roadInjuriesFile = "character", distRoadTypeFile = "character", safetyInNumbersFile = "character"), function(activeTransportFile, GBDFile, roadInjuriesFile, distRoadTypeFile, safetyInNumbersFile){
+##     ITHIM <- createITHIMFunction(activeTransportTimeFile = activeTransportFile, roadInjuriesFile = roadInjuriesFile, GBDFile = GBDFile, distRoadTypeFile = distRoadTypeFile, safetyInNumbersFile = safetyInNumbersFile)
+##     return(ITHIM)
+## })
 
-#' @rdname createITHIM-methods
-#' @aliases createITHIM
-#' @export
-setMethod("createITHIM", signature(activeTransportFile = "character", GBDFile = "character", roadInjuriesFile = "missing", distRoadTypeFile = "character", safetyInNumbersFile = "character"), function(activeTransportFile, GBDFile, roadInjuriesFile, distRoadTypeFile, safetyInNumbersFile){
-    ITHIM <- createITHIMFunction(activeTransportTimeFile = activeTransportFile, GBDFile = GBDFile, distRoadTypeFile = distRoadTypeFile, safetyInNumbersFile = safetyInNumbersFile)
-    return(ITHIM)
-})
+## #' @rdname createITHIM-methods
+## #' @aliases createITHIM
+## #' @export
+## setMethod("createITHIM", signature(activeTransportFile = "character", GBDFile = "character", roadInjuriesFile = "missing", distRoadTypeFile = "character", safetyInNumbersFile = "character"), function(activeTransportFile, GBDFile, roadInjuriesFile, distRoadTypeFile, safetyInNumbersFile){
+##     ITHIM <- createITHIMFunction(activeTransportTimeFile = activeTransportFile, GBDFile = GBDFile, distRoadTypeFile = distRoadTypeFile, safetyInNumbersFile = safetyInNumbersFile)
+##     return(ITHIM)
+## })
 
-#' @rdname createITHIM-methods
-#' @aliases createITHIM
-#' @export
-setMethod("createITHIM", signature(activeTransportFile = "character", GBDFile = "character", roadInjuriesFile = "missing", distRoadTypeFile = "missing", safetyInNumbersFile = "missing"), function(activeTransportFile, GBDFile, roadInjuriesFile, distRoadTypeFile, safetyInNumbersFile){
-    ITHIM <- createITHIMFunction(activeTransportTimeFile = activeTransportFile, GBDFile = GBDFile)
-    return(ITHIM)
-})
+## #' @rdname createITHIM-methods
+## #' @aliases createITHIM
+## #' @export
+## setMethod("createITHIM", signature(activeTransportFile = "character", GBDFile = "character", roadInjuriesFile = "missing", distRoadTypeFile = "missing", safetyInNumbersFile = "missing"), function(activeTransportFile, GBDFile, roadInjuriesFile, distRoadTypeFile, safetyInNumbersFile){
+##     ITHIM <- createITHIMFunction(activeTransportTimeFile = activeTransportFile, GBDFile = GBDFile)
+##     return(ITHIM)
+## })
 
-#' @rdname createITHIM-methods
-#' @aliases createITHIM
-#' @export
-setMethod("createITHIM", signature(activeTransportFile = "character", GBDFile = "missing", roadInjuriesFile = "missing", distRoadTypeFile = "character", safetyInNumbersFile = "character"), function(activeTransportFile, GBDFile, roadInjuriesFile, distRoadTypeFile, safetyInNumbersFile){
-    ITHIM <- createITHIMFunction(activeTransportTimeFile = activeTransportFile, distRoadTypeFile = distRoadTypeFile, safetyInNumbersFile = safetyInNumbersFile)
-    return(ITHIM)
-})
+## #' @rdname createITHIM-methods
+## #' @aliases createITHIM
+## #' @export
+## setMethod("createITHIM", signature(activeTransportFile = "character", GBDFile = "missing", roadInjuriesFile = "missing", distRoadTypeFile = "character", safetyInNumbersFile = "character"), function(activeTransportFile, GBDFile, roadInjuriesFile, distRoadTypeFile, safetyInNumbersFile){
+##     ITHIM <- createITHIMFunction(activeTransportTimeFile = activeTransportFile, distRoadTypeFile = distRoadTypeFile, safetyInNumbersFile = safetyInNumbersFile)
+##     return(ITHIM)
+## })
 
-#' @rdname createITHIM-methods
-#' @aliases createITHIM
-#' @export
-setMethod("createITHIM", signature(activeTransportFile = "character", GBDFile = "missing", roadInjuriesFile = "character", distRoadTypeFile = "character", safetyInNumbersFile = "character"), function(activeTransportFile, GBDFile, roadInjuriesFile, distRoadTypeFile, safetyInNumbersFile){
-    ITHIM <- createITHIMFunction(activeTransportTimeFile = activeTransportFile, roadInjuriesFile = roadInjuriesFile, distRoadTypeFile = distRoadTypeFile, safetyInNumbersFile = safetyInNumbersFile)
-    return(ITHIM)
-})
+## #' @rdname createITHIM-methods
+## #' @aliases createITHIM
+## #' @export
+## setMethod("createITHIM", signature(activeTransportFile = "character", GBDFile = "missing", roadInjuriesFile = "character", distRoadTypeFile = "character", safetyInNumbersFile = "character"), function(activeTransportFile, GBDFile, roadInjuriesFile, distRoadTypeFile, safetyInNumbersFile){
+##     ITHIM <- createITHIMFunction(activeTransportTimeFile = activeTransportFile, roadInjuriesFile = roadInjuriesFile, distRoadTypeFile = distRoadTypeFile, safetyInNumbersFile = safetyInNumbersFile)
+##     return(ITHIM)
+## })
 
-#' @rdname createITHIM-methods
-#' @aliases createITHIM
-#' @export
-setMethod("createITHIM", signature(activeTransportFile = "missing", GBDFile = "missing", roadInjuriesFile = "missing", distRoadTypeFile = "missing", safetyInNumbersFile = "missing"), function(activeTransportFile, GBDFile, roadInjuriesFile, distRoadTypeFile, safetyInNumbersFile){
-    ITHIM <- createITHIMFunction()
-    return(ITHIM)
-})
+## #' @rdname createITHIM-methods
+## #' @aliases createITHIM
+## #' @export
+## setMethod("createITHIM", signature(activeTransportFile = "missing", GBDFile = "missing", roadInjuriesFile = "missing", distRoadTypeFile = "missing", safetyInNumbersFile = "missing"), function(activeTransportFile, GBDFile, roadInjuriesFile, distRoadTypeFile, safetyInNumbersFile){
+##     ITHIM <- createITHIMFunction()
+##     return(ITHIM)
+## })
