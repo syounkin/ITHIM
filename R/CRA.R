fd576331855e6ebe5f664ffbb30ae3195d8f9f1b
Samuel Younkin <samuel.younkin@gmail.com>
2018-03-07 12:24:09 -0600

    Created CRA function (and file)

---
+++ R/CRA.R
@@ -0,0 +1,26 @@
+CRA <- function(meanlog.baseline = NA, sdlog.baseline = 1, meanlog.scenario = meanlog.baseline, sdlog.scenario = sdlog.baseline, n = 5, B = 1e3, P, Q, R = function(x) exp(-x), type = "parametric"){
+
+
+
+    if(type == "parametric"){
+        TA.sample.baseline <- rlnorm(n = B, meanlog = meanlog.baseline, sdlog = sdlog.baseline)
+        runif <- runif(n = B)
+
+        TA.baseline <- ifelse(runif < p0, 0, TA.sample)
+
+        P <- quantile(TA.baseline, probs = (1:n)/n)
+
+                TA.sample.scenario <- rlnorm(n = B, meanlog = meanlog.scenario, sdlog = sdlog.scenario)
+        runif <- runif(n = B)
+
+        TA.scenario <- ifelse(runif < p0, 0, TA.sample)
+
+        Q <- quantile(TA.scenario, probs = (1:n)/n)
+
+
+    }
+
+
+    return((sum(R(P)) - sum(Q(P)))/sum(R(P)))
+}
+
