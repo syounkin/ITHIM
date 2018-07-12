003e0c21d11c180dcf563e42ca22276b730f4aac
Samuel Younkin <samuel.younkin@gmail.com>
2018-07-11 21:26:13 -0500

    Changed classes for houseID, subjectID and sex

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -15,17 +15,17 @@ setClass(Class = "TravelSurvey", representation = representation(person = "data.
 
              if( length(names(object@person)) == 0 ) msg <- "Person data frame needs columns."
              if( is.null(msg) && !identical( names(object@person), c("houseID","subjectID","sex","age"))) msg <- "Person data frame has the wrong column names (or wrong column order.)"
-             if( !is.integer(object@person$houseID)) msg <- "The houseID variable in the person table must be integer class."
-             if( !is.integer(object@person$subjectID)) msg <- "The subjectID variable in the person table must be integer class."
+             if( !is.character(object@person$houseID)) msg <- "The houseID variable in the person table must be character class."
+             if( !is.character(object@person$subjectID)) msg <- "The subjectID variable in the person table must be character class."
              if( !is.factor(object@person$sex)) msg <- "The sex variable in the person table must be a factor."
-             if( !is.numeric(object@person$age)) msg <- "The age variable in the person table must be numeric."
+             if( !is.factor(object@person$age)) msg <- "The age variable in the person table must be a factor."
 
-             if( !is.integer(object@trip$houseID)) msg <- "The houseID variable in the trip table must be integer class."
-             if( !is.integer(object@trip$subjectID)) msg <- "The subjectID variable in the trip table must be integer class."
+             if( !is.character(object@trip$houseID)) msg <- "The houseID variable in the trip table must be character class."
+             if( !is.character(object@trip$subjectID)) msg <- "The subjectID variable in the trip table must be character class."
              if( !is.numeric(object@trip$duration)) msg <- "The duration variable in the trip table must be numeric."
              if( !is.factor(object@trip$mode)) msg <- "The mode variable in the trip table must be a factor."
 
-             if( !is.integer(object@house$houseID)) msg <- "The houseID variable in the house table must be integer class."
+             if( !is.character(object@house$houseID)) msg <- "The houseID variable in the house table must be character class."
              if( !is.factor(object@house$location)) msg <- "The location variable in the house table must be a factor."
 
              if( length(names(object@trip)) == 0 ) msg <- "Trip data frame needs columns."
