d315f903e68656e38ca96d5f144c5571e1dd6411
Samuel Younkin <samuel.younkin@gmail.com>
2018-08-22 11:09:30 -0500

    Created subsetting method for TravelSurvey class

---
+++ R/methods-ts.R
@@ -0,0 +1,14 @@
+setMethod("[", c("TravelSurvey", "character", "missing", "ANY"),
+    ## we won't support subsetting on j; dispatching on 'drop' doesn't
+    ## make sense (to me), so in rebellion we'll quietly ignore it.
+    function(x, i, j, ..., drop=TRUE)
+{
+    ## less clever: update slot, return instance
+    ## x@slt = x@slt[i]
+    ## x
+    ## clever: by default initialize is a copy constructor, too
+    house <- x@house[x@house$location %in% i,]
+    person <- x@person %>% dplyr::filter(houseID %in% house$houseID)
+    trip <- x@trip %>% dplyr::filter(houseID %in% house$houseID)
+    initialize(x, house = house, person = person, trip = trip)
+})
